41daff2a84b3bde3e75f1e4174f0cb6edf2ff7c1
Margaret Adams <margaret@simple.com>
2019-08-08 16:17:29 -0700

    3.js

--- 3.js
+++ 3.js
@@ -5,34 +5,17 @@
 
 // What is the largest prime factor of the number 600851475143 ?
 
-//TODO: uncomment
-// var max = 600851475143 / 2;
-var max = 50
 
-// put values you've checked as prime here (not necessary, just find its multiples up to the max and put them in the set )
-var primeSet = new Set();
+var max = 600851475143;
+let movingMax = max;
 
-// collect all the non primes here
-var nonPrimeSet = new Set();
+let i = 2
 
-var largestPrime = 0;
-
-for(var i = 2; i < max; i++) {
-  // 2,3,4,5,6,7
-  if (i === 2) {
-    primeSet.add(i)
-  } else if ((i !== 2) && (i % 2 === 0)) {
-    nonPrimeSet.add(i);
-  } else {
-    primeSet.add(i);
+for(i; i <= movingMax; i++) {
+  if (movingMax % i == 0) {
+    movingMax = movingMax / i;
+    i--;
   }
 }
 
-// var numbersWeMultiplied = [2,3,4,5,7,11]
-// var nonPrimes = [4,6,8,10,12,14,16,18,20,6,9,12]
-
-
-console.log('non prime numbers', nonPrimeSet);
-console.log('prime numbers', primeSet);
-//console.log(largestPrime);
-//
+console.log(i);
